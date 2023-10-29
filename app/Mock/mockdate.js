00dd88afdbf8438547f27f3037585b9c1a0ace04
Emiliano55 <Emiliano55>
2023-10-29 18:19:35 -0300

    mock unidades, dueños inquilinos

--- app/Mock/mockdate.js
+++ app/Mock/mockdate.js
@@ -31,4 +31,66 @@ export const users = [
     isAdmin: false,
   },
   ];
+
+  export const unidades = [
+    {
+      id: 1,
+      piso: 3,
+      habilitado: true,
+      codigoEdificio: 145,
+    },
+    {
+      id: 2,
+      piso: 5,
+      habilitado: false,
+      codigoEdificio: 754,
+    },
+    {
+      id: 3,
+      piso: 2,
+      habilitado: true,
+      codigoEdificio: 107,
+    },
+    {
+      id: 4,
+      piso: 7,
+      habilitado: false,
+      codigoEdificio: 303,
+    }
+  ]
   
+export const inquilinos = [
+  {
+    id:1,
+    identificador: 14,
+    DNI: 26486899,
+  },
+  {
+    id:2,
+    identificador: 7,
+    DNI: 22776843,
+  },
+  {
+    id:3,
+    identificador: 10,
+    DNI: 29485599,
+  }
+]
+
+export const duenio = [
+  {
+    id:3,
+    identificador: 10,
+    DNI: 29485599,
+  },
+  {
+    id:3,
+    identificador: 10,
+    DNI: 29485599,
+  },
+  {
+    id:3,
+    identificador: 10,
+    DNI: 29485599,
+  }
+]
