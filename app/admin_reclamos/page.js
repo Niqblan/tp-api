75fef42832adcd296040eba2fb8e1c2494bff3f0
Emiliano <emi2007.d@hotmail.com>
2023-10-30 12:32:34 -0300

    BOTON de volver en paginas de admin

--- app/admin_reclamos/page.js
+++ app/admin_reclamos/page.js
@@ -3,7 +3,7 @@ import '../globals.css'
 import { useContext, useEffect } from 'react'
 import { AuthContext } from '../context/Context';
 import { useRouter } from 'next/navigation';
-
+import Link from 'next/link';
 
 export default function Admin_reclamos() {
 
@@ -20,6 +20,9 @@ export default function Admin_reclamos() {
   return ( 
   <div className="flex flex-col justify-center gap-10 items-center bg-[#8ec7ec] h-[100vh] w-[100vw] font-sans pb-10 " >
     <h1 className="titulo">Administrar Reclamos</h1>
+    <Link href={"/admin"} >
+            <button className='bg-[#126bf1] text-[#fff] rounded-2xl w-[150px] p-4 absolute top-5 left-5'>Volver</button>
+    </Link>
     
 
 
